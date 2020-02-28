d35dd7270696078d2f87a2b4781cb673a621d3a2
Jiangang Luo <jluo@rsmas.miami.edu>
2020-02-28 10:55:58 -0500

    final final)

--- hist.py
+++ hist.py
@@ -1,5 +1,6 @@
 #  Randomizing data, generating summary statistics, and histogram
 import numpy as np
+import matplotlib.pyplot as plt
 
 mu = 80
 sigma = 10
@@ -10,3 +11,6 @@ print ("Random Normal Array Mean Centered", x[0:10])
 
 
 print("mean",np.mean(x))
+plt.hist(x)
+plt.show()
+
